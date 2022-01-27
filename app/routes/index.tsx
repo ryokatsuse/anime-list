081d2f518cf3591fffdabb8a30ef04667ee2c1e9
Ryo Katsuse <ryokatsu0719@gmail.com>
2022-01-28 00:27:16 +0900

    feat:api fetch animelist and skeleton ui

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,51 +1,54 @@
 
 import { SampleButton } from "./button";
 import { useLoaderData } from "remix";
+import { MantineProvider } from '@mantine/core';
+import { Alert } from '@mantine/core';
+import { List } from '@mantine/core';
+import { Fragment } from "react";
+import { Skeleton } from '@mantine/core';
+import { useState } from 'react';
 
 export async function loader() {
-  let res = await fetch("https://api.github.com/gists");
+  let res = await fetch("http://api.moemoe.tokyo/anime/v1/master/2022/1");
   return res.json();
 }
 
+
 export default function Index() {
-  let gists = useLoaderData();
+  const [loading, setLoading] = useState(true);
+  let animeData = useLoaderData();
 
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <MantineProvider
+      withNormalizeCSS
+      withGlobalStyles
+      theme={{
+        fontFamily: 'Open Sans',
+        breakpoints: {
+          sm: 800,
+          lg: 1275,
+        },
+      }}>
+      <h1>2021年のアニメリスト</h1>
       <SampleButton />
       <ul>
-      {gists.map((gist: any) => (
-        <li>
-          <a href={gist.html_url}>{gist.id}</a>
-        </li>
-      ))}
-    </ul>
-    </div>
+        {animeData ? (
+          <>
+          {animeData.map((anime: any) => (
+            <li style={{marginBottom: 10}} >{anime.title}</li>
+          ))}
+          </>
+        ) : (
+          <>
+          {animeData.map((anime: any) => (
+            <Skeleton style={{marginBottom: 10}} height={20} visible={loading}>
+              <li style={{marginBottom: 10}} >{anime.title}</li>
+            </Skeleton>
+          ))}
+          </>
+        )}
+      </ul>
+
+    </MantineProvider>
   );
 }
